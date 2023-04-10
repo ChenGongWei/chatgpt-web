6951f472aef5cebbeca01d46580eeb4afedc3e46
chengongwei <1391581728@qq.com>
2023-04-10 11:03:47 +0800

    feat：调整服务端代码

--- service/src/middleware/auth.ts
+++ service/src/middleware/auth.ts
@@ -1,20 +1,31 @@
-import { isNotEmptyString } from '../utils/is'
+import jwt from 'jsonwebtoken'
+const secretKey = 'cgwf_z(xh5$kgjk3-g5#z819ho**%yp%@!%rsv)lqjy2@m_lsy'
 
-const auth = async (req, res, next) => {
-  const AUTH_SECRET_KEY = process.env.AUTH_SECRET_KEY
-  if (isNotEmptyString(AUTH_SECRET_KEY)) {
+interface ReqJwtPayload {
+  /** 剩余次数 */
+  count: number
+}
+
+const auth = (req, res, next) => {
+  const Authorization = req.header('Authorization') || ''
+  const token = Authorization.replace('Bearer ', '').trim()
+  if (!token) {
+    throw new Error('Error: 无访问权限')
+  }
+  else {
     try {
-      const Authorization = req.header('Authorization')
-      if (!Authorization || Authorization.replace('Bearer ', '').trim() !== AUTH_SECRET_KEY.trim())
-        throw new Error('Error: 无访问权限 | No access rights')
+      const decoded = jwt.verify(token, secretKey) as ReqJwtPayload
+      // 验证通过，保存相关信息，并允许客户端继续与服务端进行交互
+      console.log(`验证信息：count: ${decoded.count}`)
+      if (!decoded?.count || decoded.count <= 0)
+        throw new Error('Error: 身份验证失败')
+
       next()
     }
     catch (error) {
-      res.send({ status: 'Unauthorized', message: error.message ?? 'Please authenticate.', data: null })
+      console.log('验证失败：', error)
+      throw new Error('Error: 身份验证失败')
     }
-  }
-  else {
-    next()
   }
 }
 
